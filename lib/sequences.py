28ab5bbbf45d5944c3a6ad84d2dfe6d75900c406
Duke <dukemorise33@gmail.com>
2023-11-28 21:10:15 -0800

    completed lesson

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    if length <= 0:
+        print('[]')
+        return
+    
+    fibonacci_sum = 0
+    fibonacci_list = []
+    
+    for index in range(length):
+
+        if index <= 1:
+            fibonacci_list.append(fibonacci_sum)
+            fibonacci_sum += 1
+        else:
+            next_fibonacci_value = fibonacci_list[index - 2] + fibonacci_list[index - 1]
+            fibonacci_list.append(next_fibonacci_value)
+        
+    print(fibonacci_list)
